93f731c846f4f2c9f25e17c42a017ea50c0d3f5d
Prasanthkumarkorada <37070068+Prasanthkumarkorada@users.noreply.github.com>
2019-09-04 12:16:05 +0000

    mark as uncomment

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -12,17 +12,17 @@ namespace SeleniumNUnitParam
     {
 
 
-        [Test]
-        public void GoogleTest()
-        {
-            Driver.Navigate().GoToUrl("http://www.google.com");
-            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-            System.Threading.Thread.Sleep(5000);
-            Driver.FindElement(By.Name("btnG")).Click();
-            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                            "The text selenium doest not exist");
+//         [Test]
+//         public void GoogleTest()
+//         {
+//             Driver.Navigate().GoToUrl("http://www.google.com");
+//             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+//             System.Threading.Thread.Sleep(5000);
+//             Driver.FindElement(By.Name("btnG")).Click();
+//             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+//                                             "The text selenium doest not exist");
 
-        }
+//         }
 
         [Test]
         public void ExecuteAutomationTest()
